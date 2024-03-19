ad1d4a65194b7cb0e7c0a42d74dbfbc116cfa5ac
vic <murimimlvictor@gmail.com>
2024-03-19 17:22:02 +0300

    move player

--- help.asm
+++ help.asm
@@ -1,3 +1,21 @@
+section .data
+
+struc Ball
+    .x:          resd  1
+    .y:          resd  1
+    .speed_x:    resd  1
+    .speed_y:    resd  1
+    .radius:     resd  1
+endstruc
+
+struc Paddle
+    .x:          resd 1
+    .y:          resd 1
+    .width:      resd 1
+    .height:     resd 1
+    .speed:      resd 1
+endstruc
+
 %macro __syscall 1
     push r11
     push rcx
@@ -15,6 +33,22 @@
 %endmacro sys_exit
 
 %define sys_exit(status) __exit status
+
+%macro begin 0
+    push rbp
+    mov rbp, rsp
+%endmacro
+
+%macro __stack_pop 0
+    mov rsp, rbp
+    pop rbp
+    ret
+%endmacro
+
+%macro end 1
+    mov rax, %1
+    __stack_pop
+%endmacro
 
 %macro __init_window 3
     mov rdi, %1
@@ -45,20 +79,20 @@
 %endmacro
 
 %macro __draw_circle 4
-    mov rdi, %1
-    mov rsi, %2
-    mov rax, %3
+    mov edi, %1
+    mov esi, %2
+    mov eax, %3
     cvtsi2ss xmm0, eax
-    mov rdx, %4
+    mov edx, %4
     call DrawCircle
 %endmacro
 
 %macro __draw_rect 5
-    mov rdi, %1
-    mov rsi, %2
-    mov rdx, %3
-    mov rcx, %4
-    mov r8,  %5
+    mov edi, %1
+    mov esi, %2
+    mov edx, %3
+    mov ecx, %4
+    mov r8d, %5
     call DrawRectangle
 %endmacro
 
@@ -67,17 +101,27 @@
     call ClearBackground
 %endmacro
 
+%macro __draw_line 5
+    mov rdi, %1
+    mov rsi, %2
+    mov rdx, %3
+    mov rcx, %4
+    mov r8,  %5
+    call DrawLine
+%endmacro
+
 %define void 0
 
-%define init_window(a, b, c)    __init_window a, b, c
-%define close_window(a)         __close_window
-%define window_should_close(a)  __window_should_close
-%define set_target_fps(a)       __set_target_fps a
-%define begin_drawing(a)        __begin_drawing
-%define end_drawing(a)          __end_drawing
-%define draw_circle(a, b, c, d) __draw_circle a, b, c, d
-%define draw_rectangle(a, b, c, d, e) __draw_rect a, b, c, d, e
-%define clear_background(a)     __clear_bg a
+%define init_window(a, b, c)        __init_window a, b, c
+%define close_window(a)             __close_window
+%define window_should_close(a)      __window_should_close
+%define set_target_fps(a)           __set_target_fps a
+%define begin_drawing(a)            __begin_drawing
+%define end_drawing(a)              __end_drawing
+%define draw_circle(a, b, c, d)     __draw_circle a, b, c, d
+%define draw_rectangle(a,b,c,d,e)   __draw_rect a, b, c, d, e
+%define clear_background(a)         __clear_bg a
+%define draw_line(a,b,c,d,e)        __draw_line a, b, c, d, e
 
 extern InitWindow
 extern CloseWindow
@@ -89,6 +133,7 @@ extern EndDrawing
 extern DrawCircle
 extern DrawRectangle
 extern ClearBackground
+extern DrawLine
 
 ;If the size of the structure, in bytes, is ≤ 8, 
 ;then the the entire structure 
--- pong.asm
+++ pong.asm
@@ -4,7 +4,25 @@ section .data
 title:          db "Pong Game", 0
 screen_width:   equ 1280
 screen_height:  equ 800
+player_height:  equ 120
+player_width:   equ 25
 
+ball: istruc Ball
+    at Ball.x,       dw screen_width/2
+    at Ball.y,       dw screen_height/2
+    at Ball.speed_x, dw 7
+    at Ball.speed_y, dw 7
+    at Ball.radius,  dw 20
+iend
+
+
+player: istruc Paddle
+    at Paddle.x,         dw screen_width - player_width - 10
+    at Paddle.y,         dw screen_height/2 - player_height/2
+    at Paddle.width,     dw player_width
+    at Paddle.height,    dw player_height
+    at Paddle.speed,     dw 6
+iend
 
 section .text
 global _start
@@ -18,12 +36,16 @@ _start:
     jne .exitgloop
     begin_drawing()
 
+    clear_background(0x00000000)
+    draw_line(screen_width/2, 0, screen_width/2 ,screen_height, 0xffffffff)
+    ;draw_circle(screen_width/2, screen_height/2, 20, 0xffffffff)
+    call DrawBall
 
-    ;clear_background(0xffffffff)
-    draw_circle(600, 400, 20, 0xffffffff)
+    call UpdateBall
 
-    draw_rectangle(10,screen_height/2 - 60, 25, 120, 0xffffffff)
-    draw_rectangle(screen_width - 35,screen_height/2 - 60, 25, 120, 0xffffffff)
+    call DrawPlayer
+    
+    ;draw_rectangle(screen_width - 35,screen_height/2 - 60, 25, 120, 0xffffffff)
 
     end_drawing()
     jmp .gloop
@@ -31,6 +53,46 @@ _start:
 .exitgloop:
     close_window()
     sys_exit(0)
+
+DrawBall:
+    begin
+    draw_circle(dword[ball+Ball.x], dword[ball+Ball.y], dword[ball+Ball.radius], 0xffffffff)
+    end 0
+
+DrawPlayer:
+    begin
+       ;draw_rectangle(screen_width - 35,screen_height/2 - 60, 25, 120, 0xffffffff)
+    draw_rectangle(dword[player+Paddle.x],dword[player+Paddle.y],dword[player+Paddle.width], dword[player+Paddle.height], 0xffffffff)
+    end 0
+
+UpdateBall:
+    begin
+    mov eax, dword[ball+Ball.speed_x]
+    add dword[ball+Ball.x], eax
+    mov eax, dword[ball+Ball.speed_y]
+    add dword[ball+Ball.y], eax
+
+    mov eax, dword[ball+Ball.y]
+    cmp eax, 0
+    jg .L1
+    neg dword[ball+Ball.speed_y]
+.L1
+    add eax, dword[ball+Ball.radius]
+    cmp eax, screen_height
+    jl .L2
+    neg dword[ball+Ball.speed_y]
+.L2:
+    mov eax, dword[ball+Ball.x]
+    cmp eax, 0
+    jg .L3
+    neg dword[ball+Ball.speed_x]
+.L3:
+    add eax, dword[ball+Ball.radius]
+    cmp eax, screen_width
+    jl .L4
+    neg dword[ball+Ball.speed_x]
+.L4:
+    end 0
 
 section '.note.GNU-stack'
 
